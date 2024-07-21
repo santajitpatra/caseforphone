60fde817ad3adc3bda6d07b3fe4f02cb72242ee6
Santajit Patra <business.santajitpatra@gmail.com>
2024-07-22 04:03:35 +0530

    feat(ui): Build landing page with Home component
    feat(ui): Build landing page with Home component
    
    * Establishes the Home component as the foundation for the landing page.
    * Renders hero section promoting custom phone cases with user-uploaded images.
    * Implements Phone component to display phone templates and a customizable image.
    * Includes descriptive text, features list with checkmarks, and positive customer reviews.
    * Utilizes "lucide-react" icons and custom image constants for visuals.

---
+++ src/components/Phone.tsx
@@ -0,0 +1,40 @@
+import { Images } from "@/constants";
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? Images.phone_template_dark_edges
+            :  Images.phone_template_white_edges
+        }
+        className="pointer-events-none z-50 select-none"
+        alt="phone image"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img
+          className="object-cover min-w-full min-h-full"
+          src={imgSrc}
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
---
+++ src/constants/images.ts
@@ -0,0 +1,22 @@
+// Create an object containing image paths
+const Images = {
+  // Base URL for all images
+  dragonlogo: "http://0.thsite.top/wp-content/uploads/2024/07/dragonlogo.png",
+  your_image: "http://0.thsite.top/wp-content/uploads/2024/07/your-image.png",
+  phone_template_dark_edges:
+    "http://0.thsite.top/wp-content/uploads/2024/07/phone-template-dark-edges.png",
+  phone_template_white_edges:
+    "http://0.thsite.top/wp-content/uploads/2024/07/phone-template-white-edges.png",
+  line: "http://0.thsite.top/wp-content/uploads/2024/07/line.png",
+  democase: "http://0.thsite.top/wp-content/uploads/2024/07/democase.jpg",
+
+  // User images
+  user_1: "http://0.thsite.top/wp-content/uploads/2024/07/user-1.png",
+  user_2: "http://0.thsite.top/wp-content/uploads/2024/07/user-2.png",
+  user_3: "http://0.thsite.top/wp-content/uploads/2024/07/user-3.png",
+  user_4: "http://0.thsite.top/wp-content/uploads/2024/07/user-4.jpg",
+  user_5: "http://0.thsite.top/wp-content/uploads/2024/07/user-5.jpg",
+};
+
+// Export the object for easy access
+export default Images;
