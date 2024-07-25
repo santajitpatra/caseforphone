d6959bff6ac7766f6df7450ff3c762076a777dd5
Santajit Patra <business.santajitpatra@gmail.com>
2024-07-25 23:39:44 +0530

    feat(ui): Implement review section with testimonials
    feat(ui): Implement review section with testimonials
    
    * Adds a new section showcasing customer testimonials with ratings and images.
    * Utilizes animation libraries for a visually appealing user experience.
    * Enhances brand credibility and trust by displaying positive user feedback.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,19 @@
 // Create an object containing image paths
 const Images = {
+  // dragon images
+  dragon_1: "http://0.thsite.top/wp-content/uploads/2024/07/dragon_1.png",
+  dragon_2: "http://0.thsite.top/wp-content/uploads/2024/07/dragon_2.png",
+  dragon_3: "http://0.thsite.top/wp-content/uploads/2024/07/dragon_3.png",
+  dragon_4: "http://0.thsite.top/wp-content/uploads/2024/07/dragon_4.png",
+
+  // testimonials
+  testimonials_1: "http://0.thsite.top/wp-content/uploads/2024/07/1.jpg",
+  testimonials_2: "http://0.thsite.top/wp-content/uploads/2024/07/2.jpg",
+  testimonials_3: "http://0.thsite.top/wp-content/uploads/2024/07/3.jpg",
+  testimonials_4: "http://0.thsite.top/wp-content/uploads/2024/07/4.jpg",
+  testimonials_5: "http://0.thsite.top/wp-content/uploads/2024/07/5.jpg",
+  testimonials_6: "http://0.thsite.top/wp-content/uploads/2024/07/6.jpg",
+
   // Base URL for all images
   dragonlogo: "http://0.thsite.top/wp-content/uploads/2024/07/dragonlogo.png",
   your_image: "http://0.thsite.top/wp-content/uploads/2024/07/your-image.png",
@@ -8,6 +22,12 @@ const Images = {
   phone_template_white_edges:
     "http://0.thsite.top/wp-content/uploads/2024/07/phone-template-white-edges.png",
   line: "http://0.thsite.top/wp-content/uploads/2024/07/line.png",
+  what_people_are_buying:
+    "http://0.thsite.top/wp-content/uploads/2024/07/what-people-are-buying.png",
+  arrow: "http://0.thsite.top/wp-content/uploads/2024/07/arrow.png",
+
+  // User images case
+  clearphone: "http://0.thsite.top/wp-content/uploads/2024/07/clearphone.png",
   democase: "http://0.thsite.top/wp-content/uploads/2024/07/democase.jpg",
 
   // User images
