44d6cb66e870cd1fe479a62b599659dd62883294
Santajit Patra <business.santajitpatra@gmail.com>
2024-08-01 01:44:01 +0530

    feat(design): Implement color selection for case customization
    feat(design): Implement color selection for case customization
    
    * Introduces radio group component for users to choose a case color.
    * Defines a color palette (`COLORS`) with labels and Tailwind classNames.
    * Updates `DesignConfigurator` to display color options and update color state.
    * Renders color options as circles with corresponding background colors.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["0.thsite.top"],
+    domains: ["0.thsite.top", "utfs.io"],
   },
 };
 
--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Images } from "@/constants";
 import { cn } from "@/lib/utils";
+import { COLORS } from "@/validators/option-validator";
 import NextImage from "next/image";
+import { useState } from "react";
+import { Rnd } from "react-rnd";
+import { RadioGroup } from "@headlessui/react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -16,6 +23,12 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number];
+  }>({
+    color: COLORS[0],
+  });
+
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -35,10 +48,91 @@ const DesignConfigurator = ({
           <div
             className={cn(
               "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
-              `bg-cyan-600`
+              `bg-${options.color.tw}`
             )}
           />
         </div>
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          className="absolute z-20 border-[3px] border-primary"
+          lockAspectRatio
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+            topLeft: <HandleComponent />,
+          }}
+        >
+          <div className="relative w-full h-full">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
+      </div>
+      <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <div className="flex flex-col gap-6">
+                <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    setOptions((prev) => ({
+                      ...prev,
+                      color: val,
+                    }));
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
+                  <div className="mt-3 flex items-center space-x-3">
+                    {COLORS.map((color) => (
+                      <RadioGroup.Option
+                        key={color.label}
+                        value={color}
+                        className={({ active, checked }) =>
+                          cn(
+                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                            {
+                              [`border-${color.tw}`]: active || checked,
+                            }
+                          )
+                        }
+                      >
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            "h-8 w-8 rounded-full border border-black border-opacity-10"
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+                
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
